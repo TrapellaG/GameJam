bbc24dd111a0a294e79c22223d634a03890fab66
TrapellaG <gerardcampos95@gmail.com>
2019-10-23 17:25:19 +0200

    1.0

---
+++ Artic_gameJam/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D myRB;
+    playerStats player;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "object")
+        {
+            Destroy(collision.gameObject);
+            player.increaseCans();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        player = GetComponent<playerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        myRB.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
---
+++ Artic_gameJam/Assets/Scripts/playerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStats : MonoBehaviour
+{
+    public bool health;
+    public float temperature;
+    public int cans;
+
+    public void increaseCans()
+    {
+        Debug.Log("cans ++");
+        cans++;
+    }
+    public void DecreaseCans()
+    {
+        cans--;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
