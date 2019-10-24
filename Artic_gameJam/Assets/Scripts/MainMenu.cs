c09b94c0dff2be188313ce2a0cfcd1373cab2ea5
TrapellaG <gerardcampos95@gmail.com>
2019-10-24 12:28:53 +0200

    MainMenu

---
+++ Artic_gameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button playButton;
+    public Button controlsButton;
+    public string level = "SampleScene";
+    public Button exitButton;
+    public GameObject panel;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Button btn1 = playButton.GetComponent<Button>();
+        Button btn2 = controlsButton.GetComponent<Button>();
+        Button btn3 = exitButton.GetComponent<Button>();
+
+        btn1.onClick.AddListener(PlayGame);
+        btn2.onClick.AddListener(ShowControls);
+        btn3.onClick.AddListener(ExitGame);
+    }
+	
+	void PlayGame()
+    {
+        Application.LoadLevel(level);
+    }
+
+    void ShowControls()
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.SetActive(!panel.activeSelf);
+        }
+    }
+}
