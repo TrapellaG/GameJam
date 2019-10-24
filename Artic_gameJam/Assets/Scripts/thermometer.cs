65ce0bd772eaaee6cc01c23b1a48f0b6b94e2c2e
TrapellaG <gerardcampos95@gmail.com>
2019-10-24 11:10:44 +0200

    thermometer

---
+++ Artic_gameJam/Assets/Scripts/thermometer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class thermometer : MonoBehaviour {
+
+    Canvas temperature;
+    public Slider temperatureSlider;
+
+    // Use this for initialization
+    void Start ()
+    {
+        temperature = GetComponent<Canvas>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        temperatureSlider.value = Player.instance.temperature;		
+	}
+}
