feb9b72c3c0a2dc3ae8187a3d270b913002e9abd
TrapellaG <gerardcampos95@gmail.com>
2019-10-24 10:36:48 +0200

    global timer

---
+++ Artic_gameJam/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    Canvas timer;
+    public Text countdown;
+    public float time = 200.0f; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time -= Time.deltaTime;
+        countdown.text = ("time: " + (int)time);
+
+        if(time == 0)
+        {
+            time = 200;
+        }
+	}
+}
